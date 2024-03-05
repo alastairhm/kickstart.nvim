b2abae9a6273e4d7422dd5c1573a35338e797bc1
Alastair Montgomery <alastair.montgomery1@nhs.net>
2024-03-05 16:14:38 +0000

    Nerd Tree

--- init.lua
+++ init.lua
@@ -161,6 +161,8 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagn
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- vim.keymap.set('n', '<C-\>',  , {desc = 'Toggle Nerd Tree'})
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
@@ -357,6 +359,7 @@ require('lazy').setup {
         },
       }
       require('nvim-tree').setup()
+      vim.api.nvim_set_keymap('n', '<C-h>', ':NvimTreeToggle<cr>', { silent = true, noremap = true })
 
       -- Enable telescope extensions, if they are installed
       pcall(require('telescope').load_extension, 'fzf')
